322367341c2cc1bebe3a816eb4837beccec4c7f8
Ole August <oleastole@gmail.com>
2022-05-04 18:48:52 +0200

    feat: add loop to do instances after one and other

--- minizinc_solver.py
+++ minizinc_solver.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 import minizinc
 import utils
@@ -6,29 +7,39 @@ from matplotlib import pyplot as plt
 SOLVER = "chuffed"
 INSTANCE_NUMBER = 12
 
-# Create a MiniZinc model
-model = minizinc.Model()
+class Solver(Enum):
+    CHUFFED = "chuffed"
+    GECODE = "gecode"
 
-# Load the model from a file
-model_file_path = 'VLSIdesign.mzn'
-with open(model_file_path, 'r') as f:
-    model.add_string(f.read())
 
-# Load the data-instance from a file
-dzn_file_path = os.path.join('dzn_instances', f'ins-{INSTANCE_NUMBER}.dzn')
-full_dzn_file_path = os.path.join(os.getcwd(), dzn_file_path)
-with open(full_dzn_file_path, 'r') as f:
-    model.add_string(f.read())
+def solve(solver_type: Solver = Solver.CHUFFED, instance_number: int = 1, model_path = 'VLSIdesign.mzn', visualize = False, intermediate_solutions = False) -> None:
+    # Create a MiniZinc model
+    model = minizinc.Model()
 
-# Transform Model into a instance
-solver = minizinc.Solver.lookup(SOLVER)
-inst = minizinc.Instance(solver, model)
+    # Load the model from a file
+    with open(model_path, 'r') as f:
+        model.add_string(f.read())
 
-# Solve the instance
-result = inst.solve(intermediate_solutions=False)
-str_res = str(result).strip().split('\n')
-parsed_res = utils.parse_result(str_res)
-utils.visualize_output(parsed_res)
+    # Load the data-instance from a file
+    dzn_file_path = os.path.join('dzn_instances', f'ins-{instance_number}.dzn')
+    full_dzn_file_path = os.path.join(os.getcwd(), dzn_file_path)
+    with open(full_dzn_file_path, 'r') as f:
+        model.add_string(f.read())
 
-print(result.statistics, "\n")
-print(result)
+    # Transform Model into a instance
+    solver = minizinc.Solver.lookup(solver_type.value)
+    inst = minizinc.Instance(solver, model)
+
+    # Solve the instance
+    result = inst.solve(intermediate_solutions=False)
+    str_res = str(result).strip().split('\n')
+    parsed_res = utils.parse_result(str_res)
+    
+    if visualize:
+        utils.visualize_output(parsed_res)
+
+    print(result.statistics, "\n")
+    print(result)
+
+for i in range(5, 15):
+    solve(instance_number = i, visualize=True)
