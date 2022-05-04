050761411feeea52bd3578adb6591781be3c244f
Ole August <oleastole@gmail.com>
2022-05-04 18:11:20 +0200

    feat: visualize output

--- utils.py
+++ utils.py
@@ -1,4 +1,34 @@
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import math
+
+def parse_result(output):
+    arr = []
+    for line in output:
+        elm = line.strip().split(' ')
+        int_elm = [int(elm) for elm in elm]
+        arr.append(int_elm)
+    return arr
+
+def visualize_output(output):
+    w = output[0][0]
+    l = output[0][1]
+    n = output[1][0]
+
+    colors = np.array(['red', 'green', 'blue', 'yellow', 'orange', 'pink', 'purple', 'brown']*(math.ceil(n/8)))
+
+    fig = plt.figure(figsize=(w/2, l/2))
+    ax = fig.gca()
+    ax.set_xticks(np.arange(0, w+1, 1))
+    ax.set_yticks(np.arange(0, l+1, 1))
+    plt.grid()
+    for i, [width, height, x, y] in enumerate(np.array(output[2:])):
+        rect = patches.Rectangle((x, y), width, height, linewidth=3, edgecolor='black', facecolor=colors[i])
+        ax.add_patch(rect)
+    plt.show()
+    return fig
 
 def parse_instance(instance_dir = 'instances', filename = "ins-1.txt"):
     instance_dir = 'instances'
