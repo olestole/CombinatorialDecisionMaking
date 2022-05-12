4e00d665a4caad105ee6934fd427f39c99bdba89
solveijm <solveig.mohr@gmail.com>
2022-05-12 15:23:05 +0200

    refactor

--- SAT/utils.py
+++ SAT/utils.py
@@ -38,8 +38,25 @@ def display_sudoku_with_rotation(sol, w, l, dims):
     ax.set_yticks(np.arange(l))
     ax.grid()
     plt.show()
+    
+def visualize_w_color(sol, w, l, numb_circuits):
+    fig, ax = plt.subplots(figsize=(l/3, w/3))
+    colors = np.array(['red', 'green', 'blue', 'Cyan', 'orange', 'black', 'purple', 'brown']*(math.ceil(numb_circuits/8)))
+    for x in range(w):
+        for y in range(l):
+            v = sol[x][y] # Mulig y, x
+            s = " "
+            if v > 0:
+                s = str(v)
+            ax.text(x+0.5, (l-0.5)-y, s, va='center', ha='center', color=colors[v])
+        ax.set_xlim(0, w)
+    ax.set_ylim(0, l)
+    ax.set_xticks(np.arange(w))
+    ax.set_yticks(np.arange(l))
+    ax.grid()
+    plt.show()
 
-def visualize_w_color(sol, w, l, dims):
+def visualize_w_color_rotation(sol, w, l, dims):
     fig, ax = plt.subplots(figsize=(l/3, w/3))
     colors = np.array(['red', 'green', 'blue', 'Cyan', 'orange', 'black', 'purple', 'brown']*(math.ceil(len(dims)/8)))
     for x in range(w):
