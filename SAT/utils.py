8a690c607b3f9d3aebbae0144363c8f835c5c363
solveijm <solveig.mohr@gmail.com>
2022-05-12 15:07:37 +0200

    feat: visualize result better

--- SAT/utils.py
+++ SAT/utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
 
 def display_sudoku(sol, w, l):
     fig, ax = plt.subplots(figsize=(4, 4))
@@ -18,7 +20,7 @@ def display_sudoku(sol, w, l):
     plt.show()
 
 def display_sudoku_with_rotation(sol, w, l, dims):
-    fig, ax = plt.subplots(figsize=(4, 4))
+    fig, ax = plt.subplots(figsize=(l/3, w/3))
     for x in range(w):
         for y in range(l):
             v = sol[x][y] # Mulig y, x
@@ -26,10 +28,31 @@ def display_sudoku_with_rotation(sol, w, l, dims):
             if v > 0:
                 numb = sol[x].count(v)
                 if numb != dims[v-1][1]:
-                    s = str(v)+"_r"
+                    s = str(v)+" R"
                 else:
                     s = str(v)
             ax.text(x+0.5, (l-0.5)-y, s, va='center', ha='center')
+        ax.set_xlim(0, w)
+    ax.set_ylim(0, l)
+    ax.set_xticks(np.arange(w))
+    ax.set_yticks(np.arange(l))
+    ax.grid()
+    plt.show()
+
+def visualize_w_color(sol, w, l, dims):
+    fig, ax = plt.subplots(figsize=(l/3, w/3))
+    colors = np.array(['red', 'green', 'blue', 'Cyan', 'orange', 'black', 'purple', 'brown']*(math.ceil(len(dims)/8)))
+    for x in range(w):
+        for y in range(l):
+            v = sol[x][y] # Mulig y, x
+            s = " "
+            if v > 0:
+                numb = sol[x].count(v)
+                if numb != dims[v-1][1]:
+                    s = str(v)+" R"
+                else:
+                    s = str(v)
+            ax.text(x+0.5, (l-0.5)-y, s, va='center', ha='center', color=colors[v])
         ax.set_xlim(0, w)
     ax.set_ylim(0, l)
     ax.set_xticks(np.arange(w))
