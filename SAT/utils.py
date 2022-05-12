96335f8fd6d06797e5cfc56f4253530d086fb458
solveijm <solveig.mohr@gmail.com>
2022-05-12 14:47:43 +0200

    feat: make possibility for rotation

--- SAT/utils.py
+++ SAT/utils.py
@@ -17,6 +17,26 @@ def display_sudoku(sol, w, l):
     ax.grid()
     plt.show()
 
+def display_sudoku_with_rotation(sol, w, l, dims):
+    fig, ax = plt.subplots(figsize=(4, 4))
+    for x in range(w):
+        for y in range(l):
+            v = sol[x][y] # Mulig y, x
+            s = " "
+            if v > 0:
+                numb = sol[x].count(v)
+                if numb != dims[v-1][1]:
+                    s = str(v)+"_r"
+                else:
+                    s = str(v)
+            ax.text(x+0.5, (l-0.5)-y, s, va='center', ha='center')
+        ax.set_xlim(0, w)
+    ax.set_ylim(0, l)
+    ax.set_xticks(np.arange(w))
+    ax.set_yticks(np.arange(l))
+    ax.grid()
+    plt.show()
+    
 def read_output(filename):
     arr = []
     with open(filename) as f:
